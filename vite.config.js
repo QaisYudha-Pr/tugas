aff0e5a7bae9b2955a7777d0988613e17649dea5
QaisYudha-Pr <qaisprawirayudha@gmail.com>
2025-02-15 10:03:44 +0700

    m

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Pastikan output ada di sini
+        emptyOutDir: true, // Membersihkan folder sebelum build baru
+    },
 });
